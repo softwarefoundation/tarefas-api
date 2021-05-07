4b9dd3c4a3e0e5514a385c1c3f962a6b50eff546
Dherkyan Ribeiro da Silva <dherkyan@gmail.com>
2021-05-07 10:04:25 -0400

    Inicio projeto tarefas-api

---
+++ src/test/java/com/softwarefoundation/tarefas/TarefasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwarefoundation.tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarefasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
