95c8209ba4fc5810e379bff1e04058894faf8230
Dherkyan Ribeiro da Silva <dherkyan@gmail.com>
2021-05-08 09:22:09 -0400

    9. Implementação da API - Habilitando o CORS

---
+++ src/main/java/com/softwarefoundation/tarefas/security/config/CorsFilterConfig.java
@@ -0,0 +1,51 @@
+package com.softwarefoundation.tarefas.security.config;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@Component
+public class CorsFilterConfig implements Filter {
+
+    @Value("${cors.uri.enabled}")
+    private String origin;
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
+        final HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        final HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", origin);
+        httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
+
+        if ("OPTIONS".equals(httpServletRequest.getMethod()) && (origin.equals(httpServletRequest.getHeader("Origin")) || "*".equals(origin))) {
+            httpServletResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
+            httpServletResponse.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+            httpServletResponse.setHeader("Access-Control-Max-Age", "3600"); // 1H
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+
+    @Override
+    public void init(final FilterConfig filterConfig) {
+
+    }
+
+}
